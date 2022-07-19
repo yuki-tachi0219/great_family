3bba1e0eb195a5574b7e0ace5d02215c5886ff16
Yuki.tachi <y.tachi0219@gmail.com>
2022-07-19 21:09:21 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 ActiveRecord::Base.transaction do
   common_last_name = "桂木"
   great_grand_parent = GreatGrandParent.create!(last_name: common_last_name, first_name: "大治郎", gender: 0)
-  birth_number_array = [2, 2, 2, 2, 2]
+  birth_number_array = [3, 3, 3, 3, 3]
   if birth_number_array.size != 5
     p "配列数不正 size=5にしてください"
     raise
@@ -10,24 +10,29 @@ ActiveRecord::Base.transaction do
     puts "数が多すぎます total = #{birth_number_array.inject(:*)} > 4000}"
     raise
   end
-  birth_number_array[0].times do
+  birth_number_array[0].times do |i|
     gender = ["male", "female"].sample
-    grand_parent = great_grand_parent.grand_parents.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.kanji, gender: gender)
+    grand_parent = great_grand_parent.grand_parents.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.hiragana, gender: gender)
+    next if i == birth_number_array[0] -1
 
-    birth_number_array[1].times do
+    birth_number_array[1].times do |j|
       gender = ["male", "female"].sample
-      parent = grand_parent.parents.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.kanji, gender: gender)
+      parent = grand_parent.parents.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.hiragana, gender: gender)
+      next if j == birth_number_array[1] -1
 
-      birth_number_array[2].times do
+      birth_number_array[2].times do |k|
         gender = ["male", "female"].sample
-        child = parent.children.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.kanji, gender: gender)
+        child = parent.children.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.hiragana, gender: gender)
+        next if k == birth_number_array[2] -1
 
-        birth_number_array[3].times do
+        birth_number_array[3].times do |l|
           gender = ["male", "female"].sample
-          grand_child = child.grand_children.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.kanji, gender: gender)
+          grand_child = child.grand_children.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.hiragana, gender: gender)
+          next if l == birth_number_array[3] -1
 
-          birth_number_array[4].times do
-            great_grand_child = grand_child.great_grand_children.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.kanji, gender: gender)
+          birth_number_array[4].times do |m|
+            great_grand_child = grand_child.great_grand_children.create!(last_name: common_last_name, first_name: Gimei.send(gender).first.hiragana, gender: gender)
+            next if m == birth_number_array[4] -1
           end
 
         end
